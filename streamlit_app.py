2056cec9c25cef2e3b98cf3e0f98b27bc6d1bf96
simonjc35 <simonjc@me.com>
2022-11-07 01:21:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('Quick Brown Fox')
 
 streamlit.header('🍞Manybe a Menu')
-streamlit.text('Omega 2 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & Rocket Sm')
-streamlit.text('hard Boiled Free-Range Eggs')
+streamlit.text('🥗Omega 2 & Blueberry Oatmeal')
+streamlit.text('🥣Kale, spinach & Rocket Sm')
+streamlit.text('🥑🍞Hard Boiled Free-Range Eggs')
