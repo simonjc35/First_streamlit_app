e2ff777ec860a762220887bf96067764db26d060
simonjc35 <simonjc@me.com>
2022-12-11 23:09:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
-
 #New Section to display Fruityvice api reposnse 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -58,9 +57,20 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+  
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur
+    my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if steamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_new_fruit)
+  streamlit.text(back_from_function)
+  
+  #streamlit.write(', add_my_fruit)
 
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into FRUIT_LOAD_LIST values('from streamlit')")
+# my_cur.execute("insert into FRUIT_LOAD_LIST values
